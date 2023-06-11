c0ce3450f30c2968d3f2a82195c46008a98d24d0
Daniel <50288378+DAguirreAg@users.noreply.github.com>
2023-06-11 02:47:04 +0200

    Create setup.sql

---
+++ source/database/setup.sql
@@ -0,0 +1,14 @@
+-- Create Finance Database
+CREATE DATABASE db_finance_universe;
+
+-- Create tables
+--- news table
+CREATE TABLE news (
+    url VARCHAR NOT NULL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    publish_date DATE NOT NULL,
+    tickers TEXT[][],
+    domain VARCHAR NOT NULL,
+    ref_filename VARCHAR NOT NULL
+);
+
